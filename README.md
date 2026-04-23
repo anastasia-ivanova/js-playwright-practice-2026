f318e13dd3f66af2fdc7b614ac9dde89d06b0176
anastasia ivanova <aivanova@amplify.com>
2026-04-23 19:37:05 +0300

    unit-10 tasks complete

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# js-playwright-practice-2026
+# playwright_tests
+
+repo that contains first playwright test
+
+## Installation
+
+```bash
+npm install
+```
+
+## Usage
+
+### Run tests:
+
+```bash
+npm run test
+```
+
+### Launch tests in UI mode:
+
+```bash
+npm run test-ui
+```
+
+### Run eslint rules and fix errors if possible:
+
+```bash
+npm run lint
+```
+
---
+++ tests/unit-10.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Unit 10 - first test', async ({ page }) => {
+  await page.goto('auth/login');
+  await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
+  await page.locator('[data-test="password"]').fill('welcome01');
+  await page.locator('[data-test="login-submit"]').click();
+  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
+  await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+});
