b1dfd32be83d70226cbca62dc04372b003028238
anastasia ivanova <aivanova@amplify.com>
2026-04-26 10:51:27 +0300

    adding fixes

--- tests/unit-10.spec.ts
+++ tests/unit-10.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('Unit 10 - first test', async ({ page }) => {
   await page.goto('auth/login');
-  await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
-  await page.locator('[data-test="password"]').fill('welcome01');
-  await page.locator('[data-test="login-submit"]').click();
+  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
+  await page.getByTestId('password').fill('welcome01');
+  await page.getByTestId('login-submit').click();
   await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
-  await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
-  await expect(page.locator('[data-test="nav-menu"]')).toContainText('Jane Doe');
+  await expect(page.getByTestId('page-title')).toContainText('My account');
+  await expect(page.getByTestId('nav-menu')).toContainText('Jane Doe');
 });
